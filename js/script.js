1c3f6cab7662b6c22a652a0a358faf2c507bc107
brucekibet <brucematwok02@gmail.com>
2025-11-15 04:56:51 +0300

    Initial upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ======= typing animation ========*/
+var typed = new Typed(".typing", {
+    strings:["Web Designer", "Web Developer", "Front-end developer","UX-UI designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
